a1b613e53d194b7cc2a7e29579da9334c97bf146
adolfrodeno <amvillalobos@uc.cl>
2021-07-01 07:33:44 -0400

    Added Ingredient-listing API

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,66 @@
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+def sample_user(email='test@test.com', password='password123'):
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password
+    )
+
+
+class PublicIngredientsAPITests(TestCase):
+    """Test the Publicly available ingredient API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required for retrieving tags"""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsAPITests(TestCase):
+    """Test the authorized user ingredients API"""
+
+    def setUp(self):
+        self.user = sample_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients(self):
+        """Test retrieving ingredients"""
+        Ingredient.objects.create(user=self.user, name='Meat')
+        Ingredient.objects.create(user=self.user, name='Salt')
+
+        res = self.client.get(INGREDIENTS_URL)
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """Test that ingredients returned are for auth user"""
+        user2 = sample_user('other@other.com', 'testpassword123')
+        Ingredient.objects.create(user=user2, name='Meat')
+        ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
